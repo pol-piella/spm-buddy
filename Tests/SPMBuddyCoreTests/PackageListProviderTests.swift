f775f40bf0fcfa786729727fbb22eeb8ebe5d766
Pol Piella Abadia <hi@polpiella.dev>
2022-09-16 14:18:45 +0100

    Adds `scan` method

--- Tests/SPMBuddyCoreTests/PackageListProviderTests.swift
+++ Tests/SPMBuddyCoreTests/PackageListProviderTests.swift
@@ -2,10 +2,14 @@ import Foundation
 import XCTest
 
 class PackageListProvider {
-    let fileManager: FileManager
+    private let fileManager: FileManager
     
     init(fileManager: FileManager = FileManager.default) {
         self.fileManager = fileManager
+    }
+    
+    func scan(directory: URL) {
+        
     }
 }
 
@@ -13,5 +17,11 @@ class PackageListProviderTests: XCTestCase {
     func testWhenProviderIsCreatedThenAFileManagerIsProvided() {
         _ = PackageListProvider(fileManager: FileManager.default)
         _ = PackageListProvider()
+    }
+    
+    func testWhenScanIsInvokedThenADirectoryPathCanBeProvided() {
+        let provider = PackageListProvider()
+        
+        provider.scan(directory: URL(string: "https://aURL.com")!)
     }
 }
