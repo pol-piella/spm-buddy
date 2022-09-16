4954f93c182cca99de3bdf0807d4de7981debcc6
Pol Piella Abadia <hi@polpiella.dev>
2022-09-16 13:58:55 +0100

    Creates a new library target to allow for easy testability

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
-        )
+        ),
+        .target(name: "SPMBuddyCore")
     ]
 )
