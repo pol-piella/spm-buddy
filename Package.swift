409b1d4c9f847b98630dbe174f517c692f136888
Pol Piella Abadia <hi@polpiella.dev>
2022-09-16 13:54:48 +0100

    Adds `swift-argument-parser` as a dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SPMBuddy",
     dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.4")
     ],
     targets: [
         .executableTarget(
