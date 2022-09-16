20733bb5b7ad317435557000654d3b8374b1bca5
Pol Piella Abadia <hi@polpiella.dev>
2022-09-16 13:53:03 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "SPMBuddy",
+    dependencies: [
+    ],
+    targets: [
+        .executableTarget(
+            name: "SPMBuddy",
+            dependencies: [])
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# SPMBuddy
+
+A description of this package.
